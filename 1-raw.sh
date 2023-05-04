6717ce617a5c4c5cf472cd1ee6516296c513d59c
Gabiru <forrest10ster@gmail.com>
2023-05-04 14:16:46 -0300

    ajuste estrutura de arquivos

---
+++ 1-raw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO
+# [ ] criar tabela address.csv
+# [ ] criar tabela customer.csv
+# [ ] criar tabela customeraddress.csv
+# [ ] criar tabela product.csv
+# [ ] criar tabela productcategory.csv
+# [ ] criar tabela productdescription.csv
+# [ ] criar tabela productmodel.csv
+# [ ] criar tabela productmodeldescription.csv
+# [ ] criar tabela salesorderdetail.csv
+# [ ] criar tabela salesorderhead.csv
---
+++ 3-visions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# TODO
+# [ ] criar tabelas com visoes de negocio
