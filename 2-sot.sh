19d332d34796f257318e92b4b790f2214e7ee503
Gabiru <forrest10ster@gmail.com>
2023-05-07 21:59:06 -0300

    ajuste camada SOT - scripts + documentacao

--- 2-sot.sh
+++ 2-sot.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# TODO
-# [ ] criar tabelas tratadas
+# CONFIG
+C1='\033[1;32m' # Color 1
+C2='\033[1;36m' # Color 2
+NC='\033[0m'    # No Color
+alias dk-exec-namenode='docker exec -it namenode bash -c '
+alias dk-exec-hive='docker exec -it hive-server bash -c '
+
+# HIVE - CREATE TABLES
+dk-exec-hive 'hive -f /mba-scripts/sot.hql'
